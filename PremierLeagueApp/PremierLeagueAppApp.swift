19865e8aaf745c74b0975e2010226da5c6b2c2fb
hafidikhsana <harifin315@gmail.com>
2022-08-06 21:53:06 +0700

    Initial Commit

---
+++ PremierLeagueApp/PremierLeagueAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PremierLeagueAppApp.swift
+//  PremierLeagueApp
+//
+//  Created by Hafid Ikhsan Arifin on 06/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PremierLeagueAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
